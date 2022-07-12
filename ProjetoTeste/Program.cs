ac6ba3bed595ac2f6bbf96880bda0bf0d8fce998
OLIMASSON <massonfellipe@hotmail.com>
2022-07-12 18:03:16 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoTeste/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutos;
+            double conta;
+
+            minutos = int.Parse(Console.ReadLine());
+
+            conta = 50.0;
+            if (minutos > 100)
+            {
+                conta = conta + (minutos - 100) * 2.0;
+            }
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+        }
+
+
+        }
+}
+
+
+
+
+
+
